5806fab3fa40df1a2885e495ebbfe484b65c7a8e
deepanshu8560 <gupta.d.deepanshu@gmail.com>
2025-09-18 10:43:43 +0530

    First Commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,12 +27,7 @@ export const Header: React.FC = () => {
 
           <div className="hidden md:flex items-center flex-1 max-w-md mx-8">
             <div className="relative w-full">
-              <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400" />
-              <input
-                type="text"
-                placeholder="Search products..."
-                className="w-full pl-10 pr-4 py-2 bg-gray-50 border border-gray-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              />
+              
             </div>
           </div>
 

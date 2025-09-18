9c81bbac1b4523eff1197a4e05e95454e3c1895f
deepanshu8560 <gupta.d.deepanshu@gmail.com>
2025-09-18 10:45:58 +0530

    First Commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,15 +31,7 @@ export const Header: React.FC = () => {
             </div>
           </div>
 
-          <div className="flex items-center gap-4">
-            <motion.button
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-              className="p-2 bg-gray-100 rounded-xl hover:bg-gray-200 transition-colors"
-            >
-              <User className="w-5 h-5 text-gray-600" />
-            </motion.button>
-          </div>
+          
         </div>
       </div>
     </motion.header>
